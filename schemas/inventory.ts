0cf426f4d48c551872cd32f99099c7e2cfb85ddf
JdesaG <guzman_jandonyjg@outlook.com>
2026-02-11 19:46:22 -0500

    borrar todo

--- schemas/inventory.ts
+++
@@ -1,9 +0,0 @@
-
-import { z } from 'zod'
-
-export const updateInventorySchema = z.object({
-    quantityAvailable: z.number().int().nonnegative().optional(),
-    isVisible: z.boolean().optional(),
-})
-
-export type UpdateInventoryInput = z.infer<typeof updateInventorySchema>
